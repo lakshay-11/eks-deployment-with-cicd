15c50dbf4ab0ff67b453464a00be3dbbe473c289
Ankit Jagtap <ankujagtap20@gmail.com>
2024-07-06 18:07:47 +0530

    changes in final name

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "welcome to Young Minds";
+		return "This is Ankit's sample project";
 	}
 
 	public static void main(String[] args) {
